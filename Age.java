85c36f0f0ff6c198888b04008663e0bf3e308187
Richa Raj <110665175+123richa-raj@users.noreply.github.com>
2024-01-06 16:11:16 +0530

    Add files via upload

---
+++ Age.java
@@ -0,0 +1,20 @@
+package Flowchart;
+import java.util.*;
+
+public class Age {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int age = sc.nextInt();
+		if(age>=18) {
+			System.out.println("Eligible for vote");
+		}
+		else {
+			System.out.println("Not Eligible for vote");
+		}
+		
+
+	}
+
+}
