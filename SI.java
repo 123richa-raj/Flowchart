e75904cbc6500b1c54fbf43ad8b100cc575f58e9
Richa Raj <110665175+123richa-raj@users.noreply.github.com>
2024-01-06 16:15:04 +0530

    Add files via upload

---
+++ SI.java
@@ -0,0 +1,17 @@
+package Flowchart;
+import java.util.*;
+
+public class SI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int p = sc.nextInt();
+		int r = sc.nextInt();
+		int t = sc.nextInt();
+		int SI = (( p * r * t)/100);
+		System.out.println(SI);
+
+	}
+
+}
